ca9e59ce2805b437782b70f1d512d4e2acd2f97e
Bram Vanroy <Bram.Vanroy@UGent.be>
2018-02-25 14:31:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# SONAR frequency lists
+ - Sorted per component (cf. [component contents below](#SONAR-components-contents))
+ - Every component dir has two sub directories:
+   - `with_pos/`: the POS tag of each keyword is included. This means that there is a distinction between homographs as 
+   long as they have different POS tags. The result is a frequency list with format `{token}\t{pos}\t{count}`
+   - `without_pos/`: the POS tag is not included. This means that there is **no** distinction between homographs. The 
+   result is a frequency list with format `{token}\t{count}`
+ - A file's name describes its content, format: 
+ `{component}[-withpos][-contentonly]-attr[-rmonlynonans][-rmonlydigits].freqs`:
+   - `component`: the SONAR component
+   - `-withpos`: whether or not POS tags are included (cf. supra)
+   - `-contentonly`: only content words are included, i.e. POS (`pt` for SONAR) of nouns (`n`), adjectives (`adj`), 
+   adverbs (`bw`), and verbs (`ww`). Other word classes are discarded
+   - `attr`: the most important: which XML attribute was used to gather frequencies from. E.g. in SONAR, for tokens 
+   `word`, for lemmata `lemma`
+   - `-rmonlynonans`: all tokens that only consist of non-alphanumeric characters have been removed from the list
+   - `-rmonlydigits`: all tokens that only consist of digits have been removed from the list (note: tokens such as 
+   `1,23` or `-3` are thus *not* removed, as it contains a non-digit character.)
+ 
+ 
+# Build your own frequency lists
+Attached script `xml_attrval_frequencies` serves as a great starting point to build your own frequency list from any 
+XML, POS-tagged corpus. Note that [**PyLT3**](https://github.com/BramVanroy/PyLT3) is required to run that script.   
+ 
+ 
+# SONAR components contents
+| Corpus   | Contents   | # sentences |
+| :------- | :--------- | ----------: |
+| WR-P-E-A | Discussion lists	4,395,094 |
+| WR-P-E-C | E-magazines | 551,119 |
+| WR-P-E-E | Newsletters | 115 |
+| WR-P-E-F | Press releases | 18,372 |
+| WR-P-E-G | Subtitles | 3,925,824 |
+| WR-P-E-H | Teletext pages | 40,715 |
+| WR-P-E-I | Websites | 204,998 |
+| WR-P-E-J | Wikipedia | 1,354,245 |
+| WR-P-E-K | Blogs | 8,614 |
+| WR-P-E-L | Tweets | 2,636,859 |
+| WR-P-P-B | Books | 1,709,808 |
+| WR-P-P-C | Brochures | 74,897 |
+| WR-P-P-D | Newsletters | 2,184 |
+| WR-P-P-E | Guides & manuals | 19,077 |
+| WR-P-P-F | Legal texts | 650,211 |
+| WR-P-P-G | Newspapers | 14,973,209 |
+| WR-P-P-H | Periodicals & magazines | 5,475,556 |
+| WR-P-P-I | Policy documents | 387,312 |
+| WR-P-P-J | Proceedings | 16,935 |
+| WR-P-P-K | Reports | 93,507 |
+| WR-U-E-A | Chats | 2,387,131 |
+| WR-U-E-D | SMS | 101,116 |
+| WR-U-E-E | Written assignments | 23,488 |
+| WS-U-E-A | Auto cues | 2,163,521 |
+| WS-U-T-B | Texts for the visually impaired | 44,862 |
+| **SoNaR** treebank | **Complete treebank** | **41,258,769** |
---
+++ xml_attrval_frequencies.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+from PyLT3.XmlHelpers import get_attr_frequencies
+from PyLT3.FileHelpers import scan_dir_and_execute, print_tuplelist
+from PyLT3.TypeHelpers import sort_simple_dict, clean_simple_dict
+
+"""
+usage: xml_tag_frequencies.py [-h] [-c] [-p] input_dir tag attr
+
+positional arguments:
+  input_dir           main directory that contains corpus XML files
+  tag                 XML tag that contains the XML attribute (in Alpino XML,
+                      that is `node`)
+  attr                XML attribute whose value is used to create the
+                      frequency list
+
+optional arguments:
+  -h, --help          show this help message and exit
+  -c, --content_only  flag to only calculate frequencies of content words
+                      (nouns, adjectives, adverbs, verbs)
+  -p, --include_pos   flag to also include the POS value of each token in the
+                      frequency list
+"""
+
+
+def get_all_freqlists(input_file, xml_tag, xml_attr, content_only, include_pos):
+    # SONAR POS tag is `pt`
+    # Content words are POS n, adj, bw, and ww (noun, adjective, adverb, verb)
+    if content_only:
+        freqs = get_attr_frequencies(input_file, [xml_tag], xml_attr, normalize_capitalisation=True,
+                                     restrict_to_pos=['n', 'adj', 'bw', 'ww'], include_pos=include_pos, pos='pt')
+    else:
+        freqs = get_attr_frequencies(input_file, [xml_tag], xml_attr, normalize_capitalisation=True,
+                                     include_pos=include_pos, pos='pt')
+
+    # string to append to filename, is this content only or not
+    contentstr = '-contentonly' if content_only else ''
+    # string to append to filename, does the output include pos tags or not
+    withposstr = '-withpos' if include_pos else ''
+
+    # Excludes tokens that consist only of non-alphanumeric characters (e.g. punctuation, space)
+    freqs_only_nonan = clean_simple_dict(freqs, rm_only_nonan=True)
+    # Excludes tokens that consist only of digits
+    freqs_only_nonan_only_digits = clean_simple_dict(freqs, rm_only_digits=True, rm_only_nonan=True)
+
+    # Given input files such as WRPPE.xml, get the component name WRPEE
+    component = Path(input_file).stem
+
+    # Use parent directory as corpus name 
+    corpus = Path(input_file).parts[-2]
+
+    if include_pos:
+        base = Path('frequency-lists', corpus, component, 'with_pos')
+    else:
+        base = Path('frequency-lists', corpus, component, 'without_pos')
+
+    # Create dir if it doesn't exist
+    Path(base).mkdir(parents=True, exist_ok=True)
+
+    freqs = sort_simple_dict(freqs, sort_on='values', reverse=True)
+    freqs_only_nonan = sort_simple_dict(freqs_only_nonan, sort_on='values', reverse=True)
+    freqs_only_nonan_only_digits = sort_simple_dict(freqs_only_nonan_only_digits, sort_on='values', reverse=True)
+
+    # Print dictionaries to files
+    print_tuplelist(freqs, base / f"{component}{withposstr}{contentstr}-{xml_attr}.freqs",
+                    encoding='utf-8')
+    print_tuplelist(freqs_only_nonan,
+                    base / f"{component}{withposstr}{contentstr}-{xml_attr}-rmonlynonans.freqs",
+                    encoding='utf-8')
+    print_tuplelist(freqs_only_nonan_only_digits,
+                    base / f"{component}{withposstr}{contentstr}-{xml_attr}-rmonlynonans-rmonlydigits.freqs",
+                    encoding='utf-8')
+
+    print(f"{corpus}: done!")
+
+    return None
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", help="main directory that contains corpus XML files")
+    parser.add_argument("tag", help="XML tag that contains the XML attribute (in Alpino XML, that is `node`)")
+    parser.add_argument("attr", help="XML attribute whose value is used to create the frequency list")
+
+    parser.add_argument("-c", "--content_only",
+                        help="flag to only calculate frequencies of content words (nouns, adjectives, adverbs, verbs)",
+                        action="store_true")
+    parser.add_argument("-p", "--include_pos",
+                        help="flag to also include the POS value of each token in the frequency list",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    scan_dir_and_execute(args.input_dir, lambda file: get_all_freqlists(file, args.tag, args.attr, args.content_only,
+                                                                        args.include_pos))
